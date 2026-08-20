e8266726c3db1f4c8dc88c86d50f7f0003512362
Rosella <rosella.merello@estudiantes.utec.edu.uy>
2026-08-20 15:17:58 -0300

    Add files via upload
    Desarrollo de secuencia 2 -  retroceso de leds

---
+++ problemaC_secuencia2.asm
@@ -0,0 +1,32 @@
+.include "m328pdef.inc"
+.org 0x0000
+rjmp reset
+
+reset:
+ldi r16, 0xFF
+out DDRD, r16
+
+ldi r17, 0b10000000
+main:
+out PORTD, r17
+rcall retardo
+lsr r17
+brne main
+ldi r17, 0b10000000
+rjmp main
+
+retardo:
+ldi r20, 20
+RET1:
+ldi r21, 255
+RET2:
+ldi r22, 255
+RET3:
+dec r22
+brne RET3
+dec r21
+brne RET2
+dec r20
+brne RET1
+
+ret
